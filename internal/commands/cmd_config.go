5d5cb250f35ab0ccbe6f4de6069bbbdd14e75785
mauricio riquelme <m.riquelme223@gmail.com>
2021-11-22 18:57:44 +1100

    adding config comman

--- internal/commands/cmd_config.go
+++ internal/commands/cmd_config.go
@@ -21,4 +21,19 @@ func config(cmd *cobra.Command, args []string) {
 		log.Println("personal access token is required")
 		return
 	}
+	config := Config{Token: args[0]}
+	if storeService.Exists() {
+		if err := storeService.Save(&config); err != nil {
+			log.Println("Error saving token")
+		}
+		return
+	}
+	if err := storeService.Create(); err != nil {
+		log.Println("Error creating config file")
+		return
+	}
+	if err := storeService.Save(&config); err != nil {
+		log.Println("Error saving token")
+		return
+	}
 }
---
+++ internal/commands/types.go
@@ -0,0 +1,5 @@
+package commands
+
+type Config struct {
+	Token string `json:"token,omitempty"`
+}
